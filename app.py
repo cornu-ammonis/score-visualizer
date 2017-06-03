00828100f034734e83a986c91966532566bcb6c2
Andrew Jones <andrewjones232@gmail.com>
2017-06-03 18:02:27 -0400

    test persisting user score

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from bokeh.embed import components
 import os
 import socket
 import time
+import datetime
 
 # Connect to Redis
 redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
@@ -26,7 +27,7 @@ def hello():
 
 	# create a new plot with a title and axis labels
 	p = figure(title="simple line example", x_axis_label='date',
-              x_axis_type='datetime', y_axis_label='score')
+              , y_axis_label='score')
 
 	# add a line renderer with legend and line thickness
 	p.line(x, y, legend="User1", line_width=2)
@@ -34,10 +35,12 @@ def hello():
 	script, div = components(p)
 	return render_template('graph.html', script=script, div=div, visits=visits)
 
-@app.route('/updatescore/<user>/<int:score>')
+@app.route('/updatescore/<user>/<score>')
 def updateScore(user, score):
 
-	r.zadd(user, time.time(), score)
+	redis.zadd(user, time.time(), int(score))
+	t = redis.zrange(user, 0, -1)
+	return render_template('graph.html', visits=str(t[0]))
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=80)
