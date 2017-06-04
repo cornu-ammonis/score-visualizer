0c439b8b3314aa7f01d87d8f3203e93bad8595a1
Andrew Jones <andrewjones232@gmail.com>
2017-06-04 13:51:14 -0400

    refactored redis configuration

--- app.py
+++ app.py
@@ -1,33 +1,36 @@
 from flask import Flask
 from flask import render_template
-from redis import Redis, RedisError
+#from redis import Redis, RedisError
 from bokeh.plotting import figure
 from bokeh.embed import components 
 import os
 import socket
 import time
 import datetime
+import redis
 
 # Connect to Redis
-redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+r = redis.StrictRedis(connection_pool = pool, decode_responses=True)
+#redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
 	try:
-		visits = redis.incr("counter")
+		visits = r.incr("counter")
 	except RedisError:
 		visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-	y = redis.zrange('test', 0, -1)
+	y = r.zrange('test', 0, -1)
 	x = []
 	for _y in y:
-		x.append(redis.zscore('test', _y))
+		x.append(r.zscore('test', _y))
 
 	# create a new plot with a title and axis labels
 	p = figure(title="simple line example", x_axis_label='date',
-              , y_axis_label='score')
+               y_axis_label='score')
 
 	# add a line renderer with legend and line thickness
 	p.line(x, y, legend="User1", line_width=2)
@@ -35,12 +38,12 @@ def hello():
 	script, div = components(p)
 	return render_template('graph.html', script=script, div=div, visits=visits)
 
-@app.route('/updatescore/<user>/<score>')
+@app.route('/updatescore/<user>/<int:score>')
 def updateScore(user, score):
 
-	redis.zadd(user, time.time(), int(score))
-	t = redis.zrange(user, 0, -1)
-	return render_template('graph.html', visits=str(t[0]))
+	r.zadd(user, time.time(), score)
+	t = r.zrange(user, 0, -1)
+	return render_template('graph.html', visits=int(t[0]))
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=80)
