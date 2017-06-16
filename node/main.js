0e063373afe8c4c904477c0dfc2f2fca7937a274
Andrew Jones <andrewjones232@gmail.com>
2017-06-16 16:10:43 -0400

    testing repository

--- node/main.js
+++ node/main.js
@@ -3,26 +3,27 @@
 const express = require('express');
 const app = express();
 let fs = require('fs');
-
+let repository = require('repository.js');
+console.log(repository.getCurrentDateString());
 
 app.set('view engine', 'pug');
 
- var data = {users: ['joe', 'jon', 'gerg'], points: []};
+var data = {users: ['joe', 'jon', 'gerg'], points: []};
 var data1 = [];
 var data2 = []
-data1[0] = {date: "2014-01-01-06-05", value: 10 };
-data1[1] = {date: "2014-01-02-02-07", value: 15 };
-data1[2] = {date: "2014-01-03-03-07", value: 30 };
-data1[3] = {date: "2014-01-05-06-05", value: 35 };
-data1[4] = {date: "2014-01-06-02-07", value: 36 };
-data1[5] = {date: "2014-01-06-10-07", value: 45 };
+data1[0] = {date: "2017-01-01-06-05", value: 10 };
+data1[1] = {date: "2017-01-02-02-07", value: 15 };
+data1[2] = {date: "2017-01-03-03-07", value: 30 };
+data1[3] = {date: "2017-01-05-06-05", value: 35 };
+data1[4] = {date: "2017-01-06-02-07", value: 36 };
+data1[5] = {date: "2017-01-06-10-07", value: 45 };
 
-data2[0] = {date: "2014-01-01-06-05", value: 10 };
-data2[1] = {date: "2014-01-02-02-07", value: 15 };
-data2[2] = {date: "2014-01-03-03-07", value: 30 };
-data2[3] = {date: "2014-01-04-06-05", value: 37 };
-data2[4] = {date: "2014-01-05-02-07", value: 40 };
-data2[5] = {date: "2014-01-06-03-07", value: 60 };
+data2[0] = {date: "2017-01-01-06-05", value: 10 };
+data2[1] = {date: "2017-01-02-02-07", value: 15 };
+data2[2] = {date: "2017-01-03-03-07", value: 30 };
+data2[3] = {date: "2017-01-04-06-05", value: 37 };
+data2[4] = {date: "2017-01-05-02-07", value: 40 };
+data2[5] = {date: "2017-01-06-03-07", value: 60 };
 
 data.points.push(data1);
 data.points.push(data2);
