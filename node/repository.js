282f127ace894e94a7d63fd82ac5cf16d175c770
Andrew Jones <andrewjones232@gmail.com>
2017-06-16 16:36:03 -0400

    implemented addScoreToUser repository function

--- node/repository.js
+++ node/repository.js
@@ -4,11 +4,37 @@ module.exports = {
 	getCurrentDateString: function() {
 		let date = new Date();
 		let dateString = "" + date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + "-" + 
-			(date.getHour() + 1) + "-" + (date.getMinute() + 1);
-	}
-	/*addScoreToUser : function(user, score) {
-		var d = new Date();
+			(date.getHours() + 1) + "-" + (date.getMinutes() + 1);
 
-		let dateString = "" + d.getFullYear() + "-" + 
-	}*/
+		return dateString;
+	}, 
+	
+	addScoreToUser : function(user, score) {
+		let dateString = this.getCurrentDateString();
+
+		if(!this.fs.existsSync('./data/userscores.json')) {
+			let data = {users: [], points: []};
+			data.users.push(user);
+
+			points = []; 
+			points[0] = {date: dateString, value: score};
+			data.points.push(points);
+			fs.writeFileSync('./data/userscores.json', JSON.stringify(data));
+		}
+		else {
+			let data = require('./data/userscores.json');
+			let userAlreadyExisted = false;
+			for (let i = 0; i < data.users.length; i++) {
+				if (data.users[i] === user) {
+					userAlreadyExisted = true;
+					data.points[i].push({date: dateString, value: score});
+				}
+			}
+
+			if (!userAlreadyExisted) {
+				data.users.push(user);
+				data.points.push([{date: dateString, value: score}]);
+			}
+		}
+	}
 }
